be9bc827e3962f03f29d49d0294be1ba9351976f
Vassilis Asim <132842654+AlchemyTweaks@users.noreply.github.com>
2024-08-25 21:58:21 +0300

    Create Revert Nvidia Telemetry.bat

---
+++ Nvidia/Nvidia Telemetry/Revert Nvidia Telemetry.bat
@@ -0,0 +1,5 @@
+@echo off
+reg add "HKLM\SOFTWARE\NVIDIA Corporation\NvControlPanel2\Client" /v "OptInOrOutPreference" /t REG_DWORD /d "1" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\nvlddmkm\Global\Startup" /v "SendTelemetryData" /t REG_DWORD /d "1" /f
+echo Execution is done.
+pause
