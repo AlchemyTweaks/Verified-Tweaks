db50ce3c336a8209d6a805fd959b498f3d97ea04
Vassilis Asimakopoulos <132842654+AlchemyTweaks@users.noreply.github.com>
2024-10-07 12:25:12 +0300

    Create Revert Direct3DTweaks(AMD).bat

---
+++ DirectX/Direct3DTweaks/AMD/Revert Direct3DTweaks(AMD).bat
@@ -0,0 +1,9 @@
+@echo off
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Direct3D" /v "DisableVidMemVBs" /f
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Direct3D" /v "FlipNoVsync" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Direct3D\Drivers" /v "SoftwareOnly" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\DirectDraw" /v "EmulationOnly" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Direct3D\Drivers" /v "SoftwareOnly" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\DirectDraw" /v "EmulationOnly" /f
+echo Completed.
+pause
